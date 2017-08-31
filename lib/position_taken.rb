b5cbce45a63153e46276254f149b9dd1af653212
Justin Phillips <justinphil02@yahoo.com>
2017-08-31 00:20:07 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,6 +2,9 @@
 def position_taken?(board, index)
 if (board[index] == " " || board[index] == "" || board[index] == nil)
 x = false
+elsif (board[index] == "X" || board[index] == "O")
+  x = true
+  
 end
 
 
